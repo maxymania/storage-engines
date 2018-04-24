6ff79f79457b3d596f48a4968ed2b29c6c5dd7a9
Simon Schmidt <simon.schmidt@htp-tel.de>
2018-04-24 10:32:39 +0200

    Update docs

--- mmdb/doc.go
+++ mmdb/doc.go
@@ -22,10 +22,16 @@ SOFTWARE.
 
 /*
 
-A memory-mapped, log-structured database with in-memory search tree (Red-Black,
-AVL or B-Tree).
+A memory-mapped, log-structured database with in-memory search tree.
+As in-memory search tree, eighter Red-Black-Tree, AVL-Tree or B-Tree can
+be choosen.
+All keys and values are stored within the memory-mapped file, the on-heap
+search tree references the keys and values within the mmap'ed file rather
+than maintaining an on-heap copy of them. This contributes to a low memory
+consumption (ignoring the mmap'ed file) in comparison to other in-memory
+indeces.
 
-The file organization:
+The file (-region) organization:
  +-------+-----------------+----------------+
  | hdr[] | Log (Records).. | unused....     |
  +-------+-----------------+----------------+
@@ -48,6 +54,20 @@ The record format:
  +--------+----------+-------------------------+
  | bytes  | value_sz | Value...                |
  +--------+----------+-------------------------+
+
+There are two indexing types, that can be used:
+ - T_Sorted
+ - T_Hashed
+
+If T_Sorted is choosen, the in-memory search tree is ordered by the keys.
+If T_Hashed is choosen, the search tree is orderes by the tuple:
+ {Farmhash32(Key), Key}
+This means, the tree is primarily ordered by the hash value and secondarily
+ordered by the String making up the key. The benefit of this is, that a
+string-comparison only takes place, if the hashes are equal (which indicates
+eighter a hash-collision or equal strings).
+As - on a cold cache - string comparisons cause IO (via page-fault), this can
+speed-up the lookups considerably.
 
 Third party go libraries used:
  - github.com/dgryski/go-farm
